0b2893ca98b1dedc90f1057a375024d38d02e800
Andrew Stakhura <andrew.stakhura@payproglobal.com>
2019-07-02 12:53:08 +0300

    fix resize, improve styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 var bodyBackgroundGradientNames = ["jshine", "evening-sunshine", "dark-ocean", "witching-hour", "kye-meh", "kyoo-tah", "sea-sunshine", "expresso", "pun-yeta", "crimson-tide" ];
 
 function centralizeContent(){
-    var element = document.querySelector("body");
-    var currentClassName = element.getAttribute("class");
-    var contentElement = document.getElementById("content");
-    var contentHeight = contentElement.offsetHeight;
-    var windowHeight = window.innerHeight;
-    if(contentHeight + 2 <= windowHeight && element.getAttribute("class").indexOf("centered") < 0) {
-        console.log(currentClassName);
-        element.setAttribute("class", currentClassName + " centered");
+  var element = document.querySelector("body");
+  var currentClassName = element.getAttribute("class");
+  var contentElement = document.getElementById("content");
+  var contentHeight = contentElement.offsetHeight;
+  var windowHeight = window.innerHeight;
+  if(contentHeight + 2 <= windowHeight)
+  {
+    if(element.getAttribute("class").indexOf("centered") < 0)
+    {
+      element.setAttribute("class", currentClassName + " centered");
     }
-    else if(element.getAttribute("class").indexOf("centered") >= 0){
-        element.setAttribute("class", currentClassName.replace("centered", "").trim());
-        // document.querySelector("body").classList.remove("centered");
-        console.log(currentClassName);
-    }
+  }
+  else {
+    element.setAttribute("class", currentClassName.replace("centered", "").trim());
+  }
 }
 
 function gradientHandler(){
-    var element = document.querySelector("body");
-    var currentClassName = element.getAttribute("class");
-    var rand =  Math.floor( Math.random() * 10);
-    element.setAttribute("class", currentClassName + " " + bodyBackgroundGradientNames[rand] + "-gradient");
-
+  var element = document.querySelector("body");
+  var currentClassName = element.getAttribute("class");
+  var rand =  Math.floor( Math.random() * 10);
+  element.setAttribute("class", currentClassName + " " + bodyBackgroundGradientNames[rand] + "-gradient");
 }
 
 window.onload = function() {
-    centralizeContent();
-    gradientHandler();
-    window.addEventListener("resize", centralizeContent);
+  centralizeContent();
+  gradientHandler();
+  window.addEventListener("resize", centralizeContent);
 }
